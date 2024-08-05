0ebc57c9f4836a3a6c105f748ff68857f81dc4a2
j.luiz <luizjaparecido@gmail.com>
2024-08-05 14:54:32 -0300

    pequena atualizacao

--- agenda.rb
+++ agenda.rb
@@ -17,11 +17,23 @@ def todos_contatos
 end
 
 def adicionar_contato
-  puts "Digite o nome do contato"
+  puts "Nome"
   nome = gets.chomp
   puts "Digite o telefone do contato"
   telefone = gets.chomp
+
   @agenda << {nome: nome, telefone: telefone}
+end
+
+def ver_contato
+  print "Qual nome você deseja ver?"
+  nome = gets.chomp
+
+  @agenda.each do |contato|
+    if contato[:nome].downcase.include?(nome.downcase)
+      puts "#{contato[:nome]} - #{contato[:telefone]}"
+    end
+  end
 end
 
 loop do
@@ -29,13 +41,15 @@ loop do
 puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
   codigo = gets.chomp.to_i
 
-  case
-  when codigo == 0
-    puts "até mais"
-    break
-  when codigo == 1
-    todos_contatos
-  when codigo == 2
-    adicionar_contato
-  end
+    case
+    when codigo == 0
+      puts "até mais"
+      break
+    when codigo == 1
+      todos_contatos
+    when codigo == 2
+      adicionar_contato
+    when codigo == 3
+      ver_contato
+    end
 end
